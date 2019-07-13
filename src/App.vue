d283fbc09c04a05780699c5368a0903ab9e7105c
Adamsh6 <adam.s.houten@gmail.com>
2019-07-13 20:15:11 +0100

    set up base game grid component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <game-grid />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameGrid from './components/GameGrid.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    "game-grid": GameGrid
   }
 }
 </script>
---
+++ src/components/GameGrid.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div>
+    <p>Here is the game grid</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "game-grid"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
